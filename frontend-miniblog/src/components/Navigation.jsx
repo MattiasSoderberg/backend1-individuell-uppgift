e2751e0e11a41456073e35bc678a52d2c5df5251
Mattias Söderberg <mattes.email@gmail.com>
2022-03-06 13:12:32 +0100

    Added logout button

--- frontend-miniblog/src/components/Navigation.jsx
+++ frontend-miniblog/src/components/Navigation.jsx
@@ -4,20 +4,21 @@ import { UserContext } from '../App'
 import { BASE_URL } from '../utils'
 
 export default function Navigation() {
-  const { user } = useContext(UserContext)
+  const { user, setUser } = useContext(UserContext)
 
   const handleLogout = () => {
-    const url = `${BASE_URL}/user/logout`
-    const token = localStorage.getItem("micro-blog")
-    const headers = {
-      "Conent-Type": "application/json",
-      "Authorization": `Bearer ${token}`
-    }
-    fetch(url, {
-      headers: headers
-    })
-    .then(res => console.log(res))
-    // localStorage.removeItem("mirco-blog")
+    // const url = `${BASE_URL}/user/logout`
+    // const token = localStorage.getItem("micro-blog")
+    // const headers = {
+    //   "Conent-Type": "application/json",
+    //   "Authorization": `Bearer ${token}`
+    // }
+    // fetch(url, {
+    //   headers: headers
+    // })
+    // .then(res => console.log(res))
+    localStorage.removeItem("micro-blog")
+    setUser(null)
   }
 
   return (
@@ -26,8 +27,9 @@ export default function Navigation() {
 
       <div className="list-group">
         <Link to="/home">Home</Link>
-        <Link to="/profile">Profile</Link>
-        <button className="btn btn-warning" onClick={handleLogout}>Logout</button>
+        {user && <Link to={`/${user.username}`}>Profile</Link>}
+        {user && <Link to="/users">Users</Link>}
+        {user && <button className="btn btn-warning" onClick={handleLogout}>Logout</button>}
         {!user && <Link to="/">Login</Link>}
       </div>
     </div>
