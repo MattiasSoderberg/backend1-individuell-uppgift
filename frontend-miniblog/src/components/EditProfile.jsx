4ab6b5be3859a0fad7ca3176f96b1b922ce69ed3
Mattias Söderberg <mattes.email@gmail.com>
2022-03-10 11:11:00 +0100

    Added firstname and lastname

--- frontend-miniblog/src/components/EditProfile.jsx
+++ frontend-miniblog/src/components/EditProfile.jsx
@@ -5,6 +5,8 @@ import { BASE_URL } from '../utils'
 export default function EditProfile() {
     const [email, setEmail] = useState("")
     const [bio, setBio] = useState("")
+    const [firstName, setFirstName] = useState("")
+    const [lastName, setLastName] = useState("")
     const [imageFile, setImageFile] = useState(null)
     const { setEditProfileActive } = useContext(UserContext)
 
@@ -20,6 +22,8 @@ export default function EditProfile() {
         const formData = new FormData()
         formData.append("email", email)
         formData.append("bio", bio)
+        formData.append("firstName", firstName)
+        formData.append("lastName", lastName)
         formData.append("imageFile", imageFile)
 
         fetch(url, {
@@ -35,6 +39,8 @@ export default function EditProfile() {
             <div className="col-3 mx-auto bg-light p-5">
                 <h3 className="mb-3">Edit Profile</h3>
                 <form action="" method='PATCH' encType="multipart/form-data">
+                    <input type="text" className="form-control mb-3" placeholder="Firstname" value={firstName} onChange={e => setFirstName(e.target.value)} />
+                    <input type="text" className="form-control mb-3" placeholder="Lastname" value={lastName} onChange={e => setLastName(e.target.value)} />
                     <input type="text" name="email" id="email" className="form-control mb-3" placeholder="email" value={email} onChange={e => setEmail(e.target.value)} />
                     <input type="text" name="bio" id="bio" className="form-control mb-3" placeholder="biography" value={bio} onChange={e => setBio(e.target.value)} />
                     <input type="file" className="form-control mb-3" onChange={e => setImageFile(e.target.files[0])} />
