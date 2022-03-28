94344f5edad67f949c86a3d575dd9ae6655af9ef
Mattias Söderberg <mattes.email@gmail.com>
2022-03-28 23:06:07 +0200

    Add comment array on post model

--- server/models/Post.js
+++ server/models/Post.js
@@ -2,10 +2,12 @@ const mongoose = require("mongoose")
 
 const postSchema = mongoose.Schema({
     text: { type: String, maxLength: 140 },
-    time: String,
+    time: Number,
     timeDisplay: String,
     author: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-    tags: [String]
+    tags: [String],
+    comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
+    likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User"}]
 }, { timestamps: true })
 
 module.exports = mongoose.model("Post", postSchema)
