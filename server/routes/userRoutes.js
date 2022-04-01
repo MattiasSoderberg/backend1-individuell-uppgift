97953c1a06045df7bccc8e8be0e16c7de28ae232
Mattias Söderberg <mattes.email@gmail.com>
2022-04-01 13:24:01 +0200

    Move logic touser controller

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,158 +1,42 @@
 const express = require("express")
-const jwt = require("jsonwebtoken")
 const multer = require("multer")
 const upload = multer({ dest: "profileImages/" })
-const User = require("../models/User")
-const Post = require("../models/Post")
-const TokenBlacklist = require("../models/TokenBlacklist")
 const { calculateTime } = require("../helpers")
 const userRouter = express.Router()
+const { userController } = require("../controllers/userController")
 const { verifyUsername, verifyToken, checkEmptyFields } = require("../middlewares/validation")
 
 
-// Requires header auth
 userRouter.get("/me", verifyToken, async (req, res) => {
-
-    const user = await User.findOne({ _id: req.user.id })
-
-    if (user) {
-        const posts = await Post.find({ author: user._id }).populate("author").sort({ time: -1 })
-        const returnData = {
-            _id: user._id,
-            firstName: user.firstName,
-            lastName: user.lastName,
-            username: user.username,
-            follows: user.follows,
-            followers: user.followers,
-            profile: user.profile,
-            posts
-        }
-        res.status(200).json(returnData)
-    } else {
-        res.status(400).json({ message: "Not found" })
-    }
+    userController.getLoggedInUser(req, res)
 })
 
-// Requires header auth
 userRouter.patch("/me", verifyToken, upload.single("imageFile"), async (req, res) => {
-    try {
-        const { id } = req.user
-        const { file } = req
-        const { firstName, lastName, email, bio } = req.body
-        const user = await User.findOne({ _id: id })
-
-        if (email) {
-            user.profile.email = email
-        }
-        if (bio) {
-            user.profile.bio = bio
-        }
-        if (firstName) {
-            user.profile.firstName = firstName
-        }
-        if (lastName) {
-            user.profile.lastName = lastName
-        }
-        if (file) {
-            user.profile.image = {
-                mimetype: file.mimetype,
-                url: file.path
-            }
-        }
-
-        await user.save()
-        res.sendStatus(200)
-    }
-    catch (err) {
-        console.log("Error catch", err)
-        res.status(400).json(err)
-    }
+    userController.updateLoggedInUser(req, res)
 })
 
-// Creates a user
-userRouter.post("/create", verifyUsername, checkEmptyFields, async (req, res) => {
-    const { password } = req.body
-    const username = req.body.username.toLowerCase()
-    const user = new User({
-        username,
-        password
-    })
-    await user.save()
-    res.sendStatus(201)
+userRouter.post("/create", checkEmptyFields, verifyUsername, async (req, res) => {
+    userController.createNewUser(req, res)
 })
 
-// username and password
 userRouter.post("/login", checkEmptyFields, async (req, res) => {
-    const { username, password } = req.body
-    const user = await User.login(username, password)
-    if (user) {
-        jwt.sign({ username, id: user._id.toString() }, process.env.JWT_SECRET, (err, token) => {
-            if (err) {
-                res.status(401).json(err)
-            } else {
-                res.status(200).json({ token })
-            }
-        })
-    } else {
-        res.status(401).json({ message: "Incorrect username or password" })
-    }
+    userController.login(req, res)
 })
 
 userRouter.get("/logout", verifyToken, async (req, res) => {
-    const activeToken = req.headers.authorization.split(" ")[1]
-    const token = new TokenBlacklist({ token: activeToken })
-
-    await token.save()
-
-    res.status(200).json({ message: `Token: ${activeToken} added to blacklist` })
+    userController.logout(req, res)
 })
 
 userRouter.get("/:username", async (req, res) => {
-    const { username } = req.params
-    const user = await User.findOne({ username })
-    if (user) {
-        const posts = await Post.find({ author: user._id }).populate("author").sort({ time: -1 })
-        posts.forEach(post => {
-            post.timeDisplay = calculateTime(post.time) || post.createdAt.toString().slice(0, 15)
-        })
-        const returnData = {
-            _id: user._id,
-            firstName: user.firstName,
-            lastName: user.lastName,
-            username: user.username,
-            follows: user.follows,
-            followers: user.followers,
-            profile: user.profile,
-            posts
-        }
-        res.status(200).json(returnData)
-    } else {
-        res.status(400).json({ message: "No user found" })
-    }
-
-
+    userController.getUserByName(req, res)
 })
 
 userRouter.get("/:id/follow", verifyToken, async (req, res) => {
-    const { id } = req.params
-    const user = await User.findById({ _id: id })
-    const followingUser = await User.findOne({ _id: req.user.id })
-    followingUser.follows.push(user._id)
-    user.followers.push(followingUser._id)
-    await followingUser.save()
-    await user.save()
-    res.status(201).json({ message: `Following user ${user._id}` })
+    userController.followUser(req, res)
 })
 
 userRouter.get("/:id/unfollow", verifyToken, async (req, res) => {
-    const { id } = req.params
-    const user = await User.findOne({ _id: id })
-    const followingUser = await User.findOne({ _id: req.user.id })
-    followingUser.follows.pull(user._id)
-    user.followers.pull(followingUser._id)
-    await followingUser.save()
-    await user.save()
-    res.status(201).json({ message: `Unfollowing user ${id}` })
+    userController.unFollowUser(req, res)
 })
 
 module.exports = userRouter
