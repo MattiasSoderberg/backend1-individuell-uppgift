4e8f1bea567fbb94798bb7a01eca3a36f3bb1955
Mattias Söderberg <mattes.email@gmail.com>
2022-04-04 16:44:25 +0200

    Refactored

--- server/routes.js
+++ server/routes.js
@@ -3,22 +3,16 @@ const jwt = require("jsonwebtoken")
 const uuid = require("uuid")
 const multer = require("multer")
 const upload = multer({ dest: "profileImages/" })
-const User = require("./models/User")
+const { getAllUsers } = require("./models/User")
 const Tag = require("./models/Tag")
 const router = express.Router()
 const { hashPassword } = require("./helpers")
 const { verifyUserName, verifyLogin, verifyToken } = require("./middlewares/validation")
 
 router.get("/users", verifyToken, async (req, res) => {
-    const users = await User.find()
+    const users = await getAllUsers()
 
     res.status(200).json(users)
-})
-
-router.get("/tags", verifyToken, async (req, res) => {
-    const tags = await Tag.find()
-
-    res.status(200).json(tags)
 })
 
 module.exports = router
