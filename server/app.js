98f9c1c51303651124ac72ef08a53eab263e7f25
Mattias Söderberg <mattes.email@gmail.com>
2022-04-04 16:44:13 +0200

    Add tags router

--- server/app.js
+++ server/app.js
@@ -7,6 +7,7 @@ require("dotenv").config()
 const userRoutes = require("./routes/userRoutes")
 const routes = require("./routes")
 const postRoutes = require("./routes/postRoutes")
+const tagRoutes = require("./routes/tagRoutes")
 const PORT = 3001
 
 mongoose.connect("mongodb://127.0.0.1:27017/micro-blog")
@@ -19,6 +20,7 @@ mongoose.connect("mongodb://127.0.0.1:27017/micro-blog")
         app.use(cors())
         app.use("/api/user", userRoutes)
         app.use("/api/posts", postRoutes)
+        app.use("/api/tags", tagRoutes)
         app.use("/api", routes)
 
         app.listen(PORT, () => {
