c17f90d0aedfb658f9d4fd5172fbe32adbd25093
Mattias Söderberg <mattes.email@gmail.com>
2022-04-04 09:19:02 +0200

    Cleanup and add find and modify tags

--- server/helpers.js
+++ server/helpers.js
@@ -1,28 +1,6 @@
-const bcrypt = require("bcrypt")
-const saltRounds = 10
+const { getTags } = require("./models/Tag")
 
 module.exports = {
-    hashPassword: (password) => {
-        return new Promise((resolve, reject) => {
-            bcrypt.genSalt(saltRounds, (err, salt) => {
-                if (err) reject(err)
-
-                bcrypt.hash(password, salt, (err, hash) => {
-                    if (err) reject(err)
-                    else resolve(hash)
-                })
-            })
-        })
-    },
-    comparePassword: (password, hash) => {
-        return new Promise((resolve, reject) => {
-            bcrypt.compare(password, hash, (err, res) => {
-                if (err) reject(err)
-                else resolve(res)
-            })
-        })
-
-    },
     calculateTime: (createdAt) => {
         const currentTime = Date.now()
 
@@ -30,5 +8,21 @@ module.exports = {
         else if ((currentTime - createdAt) / 1000 / 60 < 60) return `${Math.round((currentTime - createdAt) / 1000 / 60)} minutes ago` // Minutes
         else if ((currentTime - createdAt) / 1000 / 3600 < 24) return `${Math.round((currentTime - createdAt) / 1000 / 3600)} hours ago` // Hours
         else if ((currentTime - createdAt) / 1000 / 3600 / 24 < 7) return `${Math.round((currentTime - createdAt) / 1000 / 3600 / 24)} days ago` // Days
+    },
+    findTags: (text) => {
+        const regex = /#[\p{L}0-9]*/igu
+        const tags = text.match(regex)
+        if (tags) {
+            const trimedTags = tags.map(tag => tag.substring(1))
+            return trimedTags
+        } else {
+            return []
+        }
+    },
+    modifyNewTags: async (tags) => {
+        const existingTags = await getTags(tags)
+        const nonExistingTags = tags.filter(tag => existingTags.every(existingTag => existingTag.tag !== tag))
+        const objectifiedTags = nonExistingTags.map(tag => ({ tag }))
+        return objectifiedTags
     }
 }
